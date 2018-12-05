4b4a6c96a5ca246a1281080dd127c21c7724b181
Tuan Nguyen <me@tuannguyen.me>
2018-12-06 05:45:37 +0700

    Remove good old AppModel and reference to ScopedModel

--- lib/scoped_models/app_model.dart
+++
@@ -1,6 +0,0 @@
-import 'package:scoped_model/scoped_model.dart';
-
-import 'package:flutter_todo/scoped_models/connected_model.dart';
-
-class AppModel extends Model
-    with CoreModel, TodosModel, UserModel, SettingsModel {}
